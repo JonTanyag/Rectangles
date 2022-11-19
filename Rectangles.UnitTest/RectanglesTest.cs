98eaab1d82c8856b1bbf6307619dcbf3517ff6c8
Jon <tanyagjonjon@gmail.com>
2022-11-19 11:00:53 +0800

    update unit test

--- Rectangles.UnitTest/RectanglesTest.cs
+++ Rectangles.UnitTest/RectanglesTest.cs
@@ -1,4 +1,5 @@
-﻿using Rectangles.Core.Entity;
+﻿using System.Data.Common;
+using Rectangles.Core.Entity;
 using Rectangles.Core.Interface;
 using Rectangles.Infrastructure.Service;
 
@@ -17,7 +18,7 @@ public class RectanglesStest
     [TestCase(5,5)]
     [TestCase(10,20)]
     [TestCase(7,10)]
-    public void WhenCreatingBoard_WithCorrectDimension_ShouldReturn_True(int width, int height)
+    public void A_WhenCreatingBoard_WithCorrectDimension_ShouldReturn_True(int width, int height)
     {
         // Arrange
         var jsonService = new JsonService();
@@ -37,7 +38,7 @@ public class RectanglesStest
     [TestCase(2,2)]
     [TestCase(3,3)]
     [TestCase(7, 30)]
-    public void WhenCreatingNewBoard_WithWrongDimension_ShouldReturn_False(int width, int height)
+    public void B_WhenCreatingNewBoard_WithWrongDimension_ShouldReturn_False(int width, int height)
     {
         // Arrange
         var jsonService = new JsonService();
@@ -50,12 +51,41 @@ public class RectanglesStest
         Assert.Throws<ArgumentException>(() => { throw new ArgumentException(); });
     }
 
+
+    [TestCase(3, 2, 2, 2)]
+    [TestCase(2, 2, 4, 2)]
+    [TestCase(1, 1, 7, 4)]
+    [TestCase(1, 1, 9, 2)]
+    public void C_WhenPlacingRectangleInBoard_WithCorrectParameters_ShouldReturn_True(int width, int height, int row, int column)
+    {
+        // Arrange
+        var jsonService = new JsonService();
+        var _rectangleService = new RectangleService(jsonService);
+
+        var payload = new List<Payload>();
+
+        payload.Add(new Payload
+        {
+            Column = column,
+            Row = row,
+            Width = width,
+            Height = height,
+        });
+
+        // Act
+        var newRectangle = _rectangleService.PlaceRectangle(payload);
+
+
+        // Assert
+        Assert.IsTrue(newRectangle.Result);
+    }
+
     [Author("Jon Tanyag")]
     [TestCase(3,2, 2,2)]
     [TestCase(2,2, 1,0)]
     [TestCase(3,2, 2,2)]
     [TestCase(4,2, 2,2)]
-    public void WhenValidatingRectangle_WithCorrectDimension_ShouldReturn_True(int width, int height, int row, int column)
+    public void D_WhenValidatingRectangle_WithCorrectDimension_ShouldReturn_True(int width, int height, int row, int column)
     {
         // Arrange
         var jsonService = new JsonService();
@@ -69,12 +99,12 @@ public class RectanglesStest
         Assert.IsTrue(isValid.Result, "Rectangle within range.");
     }
 
+
     [Author("Jon Tanyag")]
-    [TestCase(3, 2, 2, 2)]
-    [TestCase(2, 2, 1, 0)]
-    [TestCase(3, 2, 2, 2)]
-    [TestCase(4, 2, 2, 2)]
-    public void WhenValidatingRectangle_WithCorrectDimension_ShouldReturn_False(int width, int height, int row, int column)
+    [TestCase(10, 2, 20, 21)]
+    [TestCase(5, 5, 0, 18)]
+    [TestCase(4, 5, 8, 2)]
+    public void E_WhenValidatingRectangle_WithCorrectDimension_ShouldReturn_False(int width, int height, int row, int column)
     {
         // Arrange
         var jsonService = new JsonService();
@@ -90,10 +120,10 @@ public class RectanglesStest
 
     [Author("Jon Tanyag")]
     [TestCase(2,2)]
-    [TestCase(1,2)]
-    [TestCase(4,4)]
-    [TestCase(24,3)]
-    public void WhenDeletingRectangle_WithIncorrectCoordinates_ShouldReturn_False(int row, int column)
+    [TestCase(4,2)]
+    [TestCase(7,4)]
+    [TestCase(9,2)]
+    public void G_WhenDeletingRectangle_WithIncorrectCoordinates_ShouldReturn_False(int row, int column)
     {
         // Arrange
         var jsonService = new JsonService();
@@ -110,10 +140,10 @@ public class RectanglesStest
 
     [Author("Jon Tanyag")]
     [TestCase(2, 2)]
-    [TestCase(1, 2)]
-    [TestCase(4, 4)]
-    [TestCase(24, 3)]
-    public void WhenDeletingRectangle_WithIncorrectCoordinates_ShouldReturn_True(int row, int column)
+    [TestCase(4, 2)]
+    [TestCase(7, 4)]
+    [TestCase(9, 2)]
+    public void F_WhenDeletingRectangle_WithCorrectCoordinates_ShouldReturn_True(int row, int column)
     {
         // Arrange
         var jsonService = new JsonService();
@@ -124,7 +154,10 @@ public class RectanglesStest
 
 
         // Assert
-        Assert.IsTrue(deleted.Result, "Rectangle not Found");
+        Assert.IsTrue(deleted.Result);
     }
+
+
+
 
 }
